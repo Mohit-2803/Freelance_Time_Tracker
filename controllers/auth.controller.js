f69eab79b5311f312060cb35a6f76b5bfebcf379
Mohit <sharmamohit2882003@gmail.com>
2025-07-24 13:09:43 +0530

    compare_password_fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -42,7 +42,7 @@ async function login(req, res) {
     if (!user) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
-    const isMatch = await User.comparePassword(password);
+    const isMatch = await user.comparePassword(password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
